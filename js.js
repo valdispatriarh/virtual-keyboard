d0fe17e11011c1b20c8d1d660d9b1899b86babd0
Vlad <valdispatriarh@gmail.com>
2023-05-01 14:15:35 +0200

    feat: add keyboard and simple button

---
+++ js.js
@@ -0,0 +1,35 @@
+document.body.onload = addElement();
+var my_div = newDiv = null;
+
+  function addElement() {
+    var newDiv = document.createElement("div");
+    newDiv.classList.toggle('klava')
+    newDiv.setAttribute('id','qwe')
+    // newDiv.innerHTML = "<h1>Привет!</h1>";
+    my_div = document.getElementById("a1");
+    document.body.insertBefore (newDiv, my_div);
+  }
+/////////////////////////////////////////////////////////////////////////////////////////
+
+let keyb = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
+
+// document.onkeypress = function (event){
+//     keyb.push(event.charCode);
+//     console.log(keyb);
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+function v27() {
+let v28 = '';
+for (let i=0; i < keyb.length; i++){
+  if (i==13){v28 += '<div class="fix"></div>';}
+  if (i==26){v28 += '<div class="fix"></div>';}
+  if (i==37){v28 += '<div class="fix"></div>';}
+
+v28 += '<div class="btn">'+ String.fromCharCode(keyb[i]) +'</div>';
+}
+document.querySelector("#qwe").innerHTML = v28;
+}v27();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
