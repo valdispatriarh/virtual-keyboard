7dccf5383daee5a561d3a9e0e2863cfa6414ae56
Vlad <valdispatriarh@gmail.com>
2023-05-01 17:50:11 +0200

    feat: add class to onclick

--- js.js
+++ js.js
@@ -7,14 +7,14 @@ var my_div = newDiv = null;
     newDiv.setAttribute('id','qwe')
     // newDiv.innerHTML = "<h1>Привет!</h1>";
     my_div = document.getElementById("a1");
-    document.body.insertBefore (newDiv, my_div);
+    document.body.insertBefore (newDiv, my_div);git
   }
-/////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
 let keyb = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
 
-// document.onkeypress = function (event){
-//     keyb.push(event.charCode);
+// document.onkeypres = function (event){
+//     keyb.push(event.key);
 //     console.log(keyb);
 // }
 
@@ -35,10 +35,26 @@ document.querySelector("#qwe").innerHTML = v28;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 document.onkeypress = function (event){
    
-    console.log(event.code);
-    console.log(event.keyCode);
+    // console.log(event.code);
+    // console.log(event.keyCode);
     document.querySelectorAll('#qwe .btn').forEach(function(element){
     element.classList.remove('push');
     });
     document.querySelector('#qwe .btn[data="' + event.keyCode + '"]').classList.add('push')
 }
+document.querySelectorAll('#qwe .btn').forEach(function(element){
+  element.onclick = function(event){
+    document.querySelectorAll('#qwe .btn').forEach(function(element){
+      element.classList.remove('push');
+      });
+      let code = this.getAttribute('data')
+      this.classList.add('push')
+  }})
+
+
+  // let keyb = [];
+
+  // document.onkeypress = function (event){
+  //     keyb.push(event.key);
+  //     console.log(keyb);
+  // }
